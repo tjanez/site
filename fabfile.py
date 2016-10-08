39a30db21b565dde193291f3fc4bdc3f2b689f49
Tadej Janež <tadej.j@nez.si>
2016-10-08 10:03:29 +0200

    Add custom Fabric command for publishing to GitHub Pages
    Augment Fabric's 'clean' command to not remove the 'output/.git'
    directory.

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,31 @@
 from fabric.api import *
 import fabric.contrib.project as project
+from fabric.contrib.console import confirm
 import os
 import shutil
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
 
+# Github Pages configuration
+env.github_pages_branch = 'master'
+
 # Port for `serve`
 PORT = 8000
 
 def clean():
     """Remove generated files"""
-    if os.path.isdir(env.deploy_path):
-        shutil.rmtree(env.deploy_path)
-        os.makedirs(env.deploy_path)
+    for root, dirs, files in os.walk(env.deploy_path):
+        for name in dirs[:]:
+            # Do not recurse into this directory
+            dirs.remove(name)
+            if name == '.git':
+                # Do not remove .git/ directory
+                pass
+            else:
+                shutil.rmtree(os.path.join(root, name))
+        for name in files:
+            os.remove(os.path.join(root, name))
 
 def build():
     """Build local version of site"""
@@ -41,3 +53,38 @@ def reserve():
 def preview():
     """Build production version of site"""
     local('py3-pelican -s publishconf.py')
+
+def gh_pages():
+    """Publish to GitHub Pages"""
+    main_git_unclean = local('git status --untracked-files=no --porcelain',
+                             capture=True)
+    if main_git_unclean:
+        abort("\n".join(["The main git repository is not clean:",
+                         main_git_unclean]))
+
+    clean()
+    # build a production version of the site
+    preview()
+
+    main_commit_sha = local('git rev-parse --short HEAD', capture=True)
+    pages_git_dir = os.path.join(os.path.abspath(env.deploy_path), '.git')
+    with lcd(env.deploy_path), shell_env(GIT_DIR=pages_git_dir):
+        pages_git_unclean = local('git status --porcelain', capture=True)
+        if pages_git_unclean:
+            local('git add --all')
+            local('git commit -m "Build of source repo @ {}"'.format(main_commit_sha))
+            if confirm("Do you wish to publish the current version of the "
+                       "page to GitHub Pages?", default=False):
+                local('git push origin {github_pages_branch}'.format(**env))
+                commit_sha = local('git rev-parse --short HEAD', capture=True)
+                puts("Pushed commit {} to GitHub Pages".format(commit_sha))
+            else:
+                # reset the git repo to the one on GitHub Pages
+                local('git reset origin/master')
+                puts("Exiting on user request.")
+        else:
+            puts("Nothing has changed. Exiting.")
+
+def publish():
+    """Publish to GitHub Pages"""
+    gh_pages()
--- publishconf.py
+++ publishconf.py
@@ -12,8 +12,6 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
-
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
