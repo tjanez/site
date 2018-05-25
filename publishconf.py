6db0db80215c235e57d372c9179e67ce367840ee
Tadej Janež <tadej.j@nez.si>
2018-05-25 10:29:37 +0200

    Change site's URL to https://tadej.ja.nez.si
    This is now possible since GitHub Pages gained support for serving
    custom domains over HTTPS. More details at:
    https://blog.github.com/2018-05-01-github-pages-custom-domains-https/.

--- publishconf.py
+++ publishconf.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://tadej.ja.nez.si'
+SITEURL = 'https://tadej.ja.nez.si'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
