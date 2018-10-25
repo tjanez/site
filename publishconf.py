74a687bfcf6450396ea71f4c87a56935cc1cc4ee
Tadej Janež <tadej.j@nez.si>
2018-10-25 17:44:37 +0200

    Revert "Change site's URL to https://tadej.ja.nez.si"
    Temporarily revert the site to plain HTTP since GitHub is having issues
    with checking CAA records for CNAME records. More details here:
    https://github.com/github/pages-health-check/pull/108.
    
    This reverts commit 6db0db80215c235e57d372c9179e67ce367840ee.

--- publishconf.py
+++ publishconf.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://tadej.ja.nez.si'
+SITEURL = 'http://tadej.ja.nez.si'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
